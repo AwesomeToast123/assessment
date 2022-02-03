eb0134cfb7d9ce9ac7cca7e25bd560e3412aaf43
niriquez <nico@symph.co>
2022-02-03 17:03:30 +0800

    Task4 progress pt 2

--- src/pages/Task4.js
+++ src/pages/Task4.js
@@ -1,15 +1,58 @@
 import React from 'react';
 
+/*
 const bem = {
    block: "",
    element: "",
    modifier: "",
    build: this.block + this.element + this.modifier
 }
+*/
 
-const Task4 = () => {
 
-  return <div></div>;
+
+function Task4(){
+
+  this.block = '';
+  this.element =  '';
+  this.modifier =  '';
+
+  return <div>{bem()}</div>;
 };
+
+Task4.prototype.block = function(block){
+     return this.block = block
+}
+
+Task4.prototype.element = function(element){
+     return this.element = element
+}
+
+Task4.prototype.modifier = function(modifier){
+     return this.modifier = modifier;
+}
+
+Task4.prototype.build = function()
+{
+   if(Task4.block === null){
+       console.log("Error empty block");
+       Task4.modifier(null);
+       Task4.element(null);
+   }else if(Task4.element === null){
+      console.log("Error empty element");
+      Task4.block(null);
+      Task4.modifier(null);
+   }else if(Task4.modifier === null){
+      console.log("Error empty modifier");
+      Task4.element(null);
+      Task4.block(null);
+   }
+}
+
+// eslint-disable-next-line no-unused-vars
+var bem = new Task4();
+Task4.block().element().modifier().build();
+
+
 
 export default Task4;
