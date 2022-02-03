5c48719f3fb27678d54d8b387913c751ec998c18
niriquez <nico@symph.co>
2022-02-03 16:46:34 +0800

    Task4 progress

--- src/pages/Task4.js
+++ src/pages/Task4.js
@@ -1,6 +1,14 @@
 import React from 'react';
 
+const bem = {
+   block: "",
+   element: "",
+   modifier: "",
+   build: this.block + this.element + this.modifier
+}
+
 const Task4 = () => {
+
   return <div></div>;
 };
 
